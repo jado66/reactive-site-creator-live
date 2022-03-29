9eba27c0932a178aad9b5c317169c5f4021f5262
jado66 <jado66@gmail.com>
2022-03-29 18:11:53 -0400

    Added baseline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Website from "reactive-site-creator/dist/components/Website";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Website
+      siteName = {"Reactive Site Creator"}
+      basename = {"/reactive-site-creator-live"}
+      isAdmin = {true}
+    />
   );
 }
+
+ 
 
 export default App;
